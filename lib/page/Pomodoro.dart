5994bc591997e0ea410e1966c010833db7fe9ffd
ber <bernardomelofacu@gmail.com>
2024-09-27 21:57:20 -0300

    POMODORO

---
+++ lib/page/Pomodoro.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:pomodoro/components/Cronometro.dart';
+import 'package:pomodoro/components/EntradaTempo.dart';
+import 'package:provider/provider.dart';
+import '../store/pomodoro.store.dart';
+
+class Pomodoro extends StatelessWidget {
+  const Pomodoro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Expanded(
+            child: Cronometro(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Observer(
+              builder: (_) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  EntradaTempo(
+                    titulo: 'Trabalho',
+                    valor: store.tempoTrabalho,
+                    inc: store.incrementarTempoTrabalho,
+                    dec: store.decrementarTempoTrabalho,
+                  ),
+                  EntradaTempo(
+                    titulo: 'Descanso',
+                    valor: store.tempoDescanso,
+                    inc: store.incrementarTempoDescanso,
+                    dec: store.decrementarTempoDescanso,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
